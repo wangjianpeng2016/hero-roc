16d90024fa1251206a8f57cb9ba4d0329d84e9f6
wangjianpeng228@163.com <Hero82726171>
2017-11-11 14:26:25 +0800

    基础部分

---
+++ roc-mybatis/src/main/java/com/iframe/base/interceptor/MapperInterceptor.java
@@ -0,0 +1,10 @@
+package com.iframe.base.interceptor;
+
+/**
+ * @Description MyBatis Mapper 拦截器
+ * @author wangjp
+ * @Date: 2017-11-11 14:25
+ */
+public class MapperInterceptor {
+
+}
---
+++ roc-mybatis/src/test/java/com/test/mybatis/main/TestMyBatisPersonMain.java
@@ -0,0 +1,4 @@
+package com.test.mybatis.main;
+
+public class TestMyBatisPersonMain {
+}
